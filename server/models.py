f30df3675f8f9ead3dc8e81f80c5436cc565b331
Emmanuel-hub-bit <emmanuelonyango467@gmail.com>
2024-10-07 17:07:34 +0300

    all

--- server/models.py
+++ server/models.py
@@ -7,3 +7,9 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    image = db.Column(db.String, nullable=True)
+    price = db.Column(db.Numeric, nullable=False)  # Use Numeric for decimal values
+
+    def __repr__(self):
+        return f'{self.name}, {self.image}, {self.price}'
